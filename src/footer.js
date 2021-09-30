92fe89d7b21735ae8f078f84e01e362524e63940
Enzo Emiliano Silva de Souza <enzoemiliano96@gmail.com>
2021-09-30 17:09:57 -0300

    footer

---
+++ src/footer.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import styled from 'styled-components'
+
+
+const FooterClass = styled.div`
+
+display: flex;
+alin-items: center;
+justify-content: space-between;
+`
+
+
+export default class Footer extends React.Component {
+
+	render(){
+		return (
+            
+            <FooterClass>
+            <h3>
+               Somos uma plataforma online para conectar clientes a prestadores de serviços por todo país.
+            </h3>
+
+            
+            <h3> Labeninjas Copyright© </h3>
+            <h3> - Desenvolvido por: <br/> Enzo Emiliano, Élida Nunes, Júlia Cortez, Mariana Trevisane e Rafael Flores -  </h3>
+            
+        </FooterClass>
+		
+		)
+	}
+}
+
