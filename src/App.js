5ad8a57d9cfdffbabbbf99d0dc59669aa668b237
Enzo Emiliano Silva de Souza <enzoemiliano96@gmail.com>
2021-10-01 12:38:28 -0300

    pagfinalninja9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import TelaCarrinho from './components/ItensPasta/CarrinhoDeCompras'
-import Filtros from './components/Filtros/Filtros'
+import Filtros from './components/Filtros/filtros'
 import { PacoteServico } from './components/ItensPasta/PacoteServicos'
 import styled from 'styled-components'
 import TelaHome from './components/paginaHome'
 // import TelaCarrinho from './components/carrinhoDeCompras'
 import PaginaCadastro from './components/PaginaCadastro'
 import PaginaServicos from './components/PaginaServicos'
-
+import Footer from './footer'
+import PagFinal from './paginaFinal'
 
 // export default class App extends React.Component {
 
@@ -160,7 +161,12 @@ export default class App extends React.Component {
 				</Home>
 				<button onClick={() => this.changePage("PaginaServicos")}>Servicos</button>
 				<button onClick={() => this.changePage("PaginaCadastro")}>Cadastro</button>
+				
+				
+			
 			{renderCurrentPage()}
+				<PagFinal></PagFinal>
+				<Footer></Footer>
 			</div>
 		)
 	}
