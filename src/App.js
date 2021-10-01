e88950bcdb2ad4b90c15abb0f01255a54d6337ad
Mariana Stamatakis <mst.nanasta@gmail.com>
2021-10-01 10:54:36 -0300

    componente botoes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Filtros from './components/Filtros/filtros'
 import { PacoteServico } from './components/ItensPasta/PacoteServicos'
 import styled from 'styled-components'
 import TelaHome from './components/paginaHome'
-// import TelaCarrinho from './components/carrinhoDeCompras'
+import Header from './components/Header'
 import PaginaCadastro from './components/PaginaCadastro'
 import PaginaServicos from './components/PaginaServicos'
 import Footer from './footer'
+
 
 // export default class App extends React.Component {
 
@@ -67,21 +68,19 @@ import Footer from './footer'
 // import PaginaServicos from './components/PaginaServicos'
 
 
-const Home = styled.div`
-    max-width: 1010px;
-    padding: 5px 15px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    margin: 0 auto;
-    color: black;
-`;
+// const Home = styled.div`
+//     max-width: 1010px;
+//     padding: 5px 15px;
+//     width: 100%;
+//     display: flex;
+//     align-items: center;
+//     margin: 0 auto;
+//     color: black;
+// `;
 
 
 export default class App extends React.Component {
 	state = {
-		currentPage: 'TelaHome',
-    	telaAtual:"TelaHome",
 		valorTotal: 550,
 		carrinho: [
 			{
@@ -114,33 +113,6 @@ export default class App extends React.Component {
 		console.log("serviço" , item)
 	}
   	
-	escolheTela = () => {
-		switch (this.state.telaAtual) {
-			case "TelaHome":
-				return <TelaHome />;
-			case "TelaServicos":
-				return <PaginaServicos />;
-			case "TelaCadastro":
-				return <PaginaCadastro />;
-			case "TelaCarrinho":
-				return <TelaCarrinho 
-				carrinho ={this.state.carrinho}
-				valorTotal ={this.state.valorTotal}
-				removerItem={this.removerItem}
-				/>;
-			default:
-				return <TelaHome />;
-		}
-	}
-
-	mudaTela = (nomeTela) =>{
-		this.setState({telaAtual: nomeTela})
-	}
-
-	changePage = (currentPage) => {
-		this.setState({ currentPage: currentPage });
-	};
-
 	render() {
 		const renderCurrentPage = () => {
 			if (this.state.currentPage === "PaginaCadastro") {
@@ -151,18 +123,9 @@ export default class App extends React.Component {
 		};
 		return (
 			<div>
-        <Home>
-				{/* <button onClick={()=> this.mudaTela("TelaHome")}>Home</button>  */}
-				<button onClick={()=> this.mudaTela("TelaServicos")}>Contrate um LabeNinja</button>
-				<button onClick={()=> this.mudaTela("TelaCadastro")}>Seja um LabeNinja</button>
-				{/* <button onClick={()=> this.mudaTela("TelaCarrinho")}>Carrinho de compras</button> */}
-				{this.escolheTela()}
-				</Home>
-				<button onClick={() => this.changePage("PaginaServicos")}>Servicos</button>
-				<button onClick={() => this.changePage("PaginaCadastro")}>Cadastro</button>
-			{renderCurrentPage()}
-
-			<Footer></Footer>
+				<Header />
+				<TelaHome/>
+				<Footer/>
 			</div>
 		
 		
